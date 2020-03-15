4c80dbe873551052a32aefc683fb997e3e90d354
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-03-15 21:09:23 +1300

    Lab 1 MinorProgres

--- Lab1/StudentT.py
+++ Lab1/StudentT.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.special import gamma
+import scipy.stats as stats
 
 def getColumn(col_num):
     return [row[col_num] for row in nums]
@@ -10,26 +11,44 @@ mean = 2.5
 variance = 16
 sigma = variance ** 0.5
 M = 5
-size = 10000
+N = 10000
 
-nums = sigma * np.random.randn(M, size) + mean
+v = M - 1
+
+nums = sigma * np.random.randn(M, N) + mean
 
 # print(nums)
 t_values = []
 
-for i in range(size):
+for i in range(N):
     col = getColumn(i)
     sample_mean = np.mean(col)
     sample_stddev = np.std(col)
     t = ((mean - sample_mean) / sample_stddev) * np.sqrt(M)
     t_values += [t]
 
-print(t_values)
+# print(t_values)
 
 # plt.hist(t_values, bins=80, range = (-10, 10))
-histVals, bins = np.histogram(t_values, bins=40, range=(-10,10))
-binSize = bins[1]-bins[0]
-plotVals = histVals / size
-plotVals = plotVals / binSize
-plt.bar(bins[:-1],plotVals,width=binSize, color="blue", alpha=0.5)
+plt.figure(1)
+H, X = np.histogram(t_values, bins=40, range=(-10,10))
+dx = X[1]-X[0]
+plotVals = H / N
+plotVals = plotVals / dx
+plt.bar(X[:-1],plotVals,width=dx, color="blue", alpha=0.5)
+
+tpdf = gamma((v+1)/2) / (np.sqrt(v*np.pi) * gamma(v/2) * np.power(1+(np.power((X+dx/2), 2)/v), (v+1)/2))  # +dx/2 just moves over the center x values
+plt.plot(X, tpdf)
+
+# plt.figure(2)
+
+tcdf = stats.t.cdf(X, N-1)
+plt.plot(X,tcdf)
+# plt.figure(2)
+# tcdf = np.cumsum(H/N)
+# plt.plot(X[1:],tcdf)
+
+# tcdf = np.cumsum(tpdf*dx)
+# plt.plot(X,tcdf)
+
 plt.show()
