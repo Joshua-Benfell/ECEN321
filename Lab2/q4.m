69c5aedf465c5120b35c5299e852e05cb94ab6e7
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-05-06 18:58:53 +1200

    Lab 2

--- Lab2/q4.m
+++ Lab2/q4.m
@@ -16,7 +16,7 @@ edges = linspace(0.55, 0.65, boundary / dx);
 
 [histVals, bins] = hist(V_3, edges);
 figure(1)
-plot(edges, histVals/N);
+bar(edges, histVals/N);
 sum(histVals/N)
 title("PDF of V_2 - V_1")
 xlabel("Voltage (V)")
@@ -27,3 +27,17 @@ std(V_2)
 
 mean(V_3)
 std(V_3)*1000
+
+figure(2)
+edges = linspace(1.93, 1.94, boundary/dx);
+[histV1, bins] = hist(V_1, edges);
+bar(edges,histV1/N, 'EdgeColor','none')
+
+hold on
+edges = linspace(2.5, 2.6, boundary/dx);
+[histV2, bins] = hist(V_2, edges);
+bar(edges,histV2/N,'EdgeColor','none')
+title("Distributions of V_1 and V_2");
+xlabel("V (V)")
+ylabel("Probabilty Density")
+legend(["V_1","V_2"])
